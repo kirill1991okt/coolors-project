5be6460507a8ca3f77d1b02ab8f9b222e5ba8837
Kirill <kirill1991okt@gmail.com>
2022-10-23 18:21:24 +0300

    feat: add functional for load page

--- app.js
+++ app.js
@@ -9,8 +9,8 @@ window.addEventListener('DOMContentLoaded', () => {
   });
 
   document.addEventListener('click', (e) => {
-    const lock = e.target.dataset.type;
-    if (lock === 'lock') {
+    const nameAtr = e.target.dataset.type;
+    if (nameAtr === 'lock') {
       const node =
         e.target.tagName.toLocaleLowerCase() === 'i'
           ? e.target
@@ -18,6 +18,8 @@ window.addEventListener('DOMContentLoaded', () => {
 
       node.classList.toggle('fa-lock-open');
       node.classList.toggle('fa-lock');
+    } else if (nameAtr === 'copy') {
+      copyToClipboard(e.target.textContent);
     }
   });
 
@@ -32,22 +34,34 @@ window.addEventListener('DOMContentLoaded', () => {
     return '#' + color;
   }
 
-  function setRandomColors() {
-    columns.forEach((col) => {
-      const color = generateRandomColor(),
-        text = col.querySelector('h2'),
+  function setRandomColors(isInitial) {
+    const colorsArr = isInitial ? getColorsFromHash() : [];
+    columns.forEach((col, index) => {
+      const text = col.querySelector('h2'),
         btn = col.querySelector('button'),
         isLocked = col.querySelector('i').classList.contains('fa-lock');
 
       if (isLocked) {
+        colorsArr.push(text.textContent);
         return;
       }
 
+      const color = isInitial
+        ? colorsArr[index]
+          ? colorsArr[index]
+          : generateRandomColor()
+        : generateRandomColor();
+
+      if (!isInitial) {
+        colorsArr.push(color);
+      }
       text.textContent = color;
       col.style.background = color;
       setTextColor(text, color);
       setTextColor(btn, color);
     });
+
+    updateColorHash(colorsArr);
   }
 
   function setTextColor(text, color) {
@@ -55,5 +69,27 @@ window.addEventListener('DOMContentLoaded', () => {
     text.style.color = luminance > 0.5 ? '#000' : '#fff';
   }
 
-  setRandomColors();
+  function copyToClipboard(text) {
+    return navigator.clipboard.writeText(text);
+  }
+
+  function updateColorHash(colors = []) {
+    document.location.hash = colors.map((color) => color.slice(1)).join('-');
+  }
+
+  function getColorsFromHash() {
+    if (document.location.hash.length > 1) {
+      return document.location.hash
+        .substring(1)
+        .split('-')
+        .map((item) => '#' + item);
+    }
+
+    return [];
+  }
+
+  // написать функцию для открытия модального окна и закрытия
+  // можно добавить окно, которое подсказывает что для смены цвета необходимо нажимать на пробел
+
+  setRandomColors(true);
 });
