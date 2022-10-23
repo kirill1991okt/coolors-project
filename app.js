1273fb98ccac02d8d27d0d36a3bc54c265e093c7
Kirill <kirill1991okt@gmail.com>
2022-10-23 16:14:25 +0300

    feat: add lock color

--- app.js
+++ app.js
@@ -1,32 +1,59 @@
-const columns = document.querySelectorAll('.col');
+window.addEventListener('DOMContentLoaded', () => {
+  const columns = document.querySelectorAll('.col');
 
-function generateRandomColor() {
-  const hexCode = '1234567890abcdef';
-  let color = '';
+  document.addEventListener('keydown', (e) => {
+    e.preventDefault();
+    if (e.code.toLocaleLowerCase() === 'space') {
+      setRandomColors();
+    }
+  });
 
-  for (let i = 0; i < 6; i++) {
-    color += hexCode[Math.floor(Math.random() * hexCode.length)];
+  document.addEventListener('click', (e) => {
+    const lock = e.target.dataset.type;
+    if (lock === 'lock') {
+      const node =
+        e.target.tagName.toLocaleLowerCase() === 'i'
+          ? e.target
+          : e.target.children[0];
+
+      node.classList.toggle('fa-lock-open');
+      node.classList.toggle('fa-lock');
+    }
+  });
+
+  function generateRandomColor() {
+    const hexCode = '1234567890abcdef';
+    let color = '';
+
+    for (let i = 0; i < 6; i++) {
+      color += hexCode[Math.floor(Math.random() * hexCode.length)];
+    }
+
+    return '#' + color;
   }
 
-  return '#' + color;
-}
+  function setRandomColors() {
+    columns.forEach((col) => {
+      const color = generateRandomColor(),
+        text = col.querySelector('h2'),
+        btn = col.querySelector('button'),
+        isLocked = col.querySelector('i').classList.contains('fa-lock');
 
-function setRandomColors() {
-  columns.forEach((col) => {
-    const color = generateRandomColor(),
-      text = col.querySelector('h2'),
-      btn = col.querySelector('button');
+      if (isLocked) {
+        return;
+      }
 
-    text.textContent = color;
-    col.style.background = color;
-    setTextColor(text, color);
-    setTextColor(btn, color);
-  });
-}
+      text.textContent = color;
+      col.style.background = color;
+      setTextColor(text, color);
+      setTextColor(btn, color);
+    });
+  }
 
-function setTextColor(text, color) {
-  const luminance = chroma(color).luminance();
-  text.style.color = luminance > 0.5 ? '#000' : '#fff';
-}
+  function setTextColor(text, color) {
+    const luminance = chroma(color).luminance();
+    text.style.color = luminance > 0.5 ? '#000' : '#fff';
+  }
 
-setRandomColors();
+  setRandomColors();
+});
